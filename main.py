6266f7c01ce8b8e2744ab390bc89945e8a914823
Amir Eldor <amir@eize.ninja>
2016-12-29 02:29:31 +0200

    Modules are loaded on runtime!

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import sys
 import time
 import logging
+import importlib.util
+import os.path
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, PatternMatchingEventHandler
+import genscripts
+
+
+genscripts_dir= 'genscripts'
 
 
 class MyHandler(PatternMatchingEventHandler):
-    def __init__(self, pattern, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, patterns, *args, **kwargs):
+        super().__init__(patterns=patterns, *args, **kwargs)
 
     def on_created(self, event):
         self.handle_event(event)
@@ -18,23 +25,24 @@ class MyHandler(PatternMatchingEventHandler):
         self.handle_event(event)
 
     def handle_event(self, event):
-        print("HI!!!!", event.src_path)
+        module_name = os.path.basename(event.src_path).rstrip('.py')
+        module = importlib.import_module('{}.{}'.format(genscripts_dir, module_name))
+        logging.debug('module {} {}'.format(module_name, module))
+        print('gen', module.generate())
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
+    sys.path.append('.{}'.format(genscripts_dir))
+    logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    logging_handler = LoggingEventHandler()
-    my_handler = MyHandler('*.py')
+    my_handler = MyHandler(patterns=['*.py'])
     observer = Observer()
-    directory = 'genscripts'
-    observer.schedule(logging_handler, directory)
-    observer.schedule(my_handler, directory)
+    observer.schedule(my_handler,genscripts_dir)
     observer.start()
     try:
-        print("Running...")
+        logging.info("Running...")
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
